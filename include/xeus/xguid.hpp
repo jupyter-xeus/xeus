f32ed52adf76bf8258b2d14882f35add438e93eb
Johan Mabille <johan.mabille@gmail.com>
2016-12-07 19:19:27 +0100

    guid

---
+++ include/xeus/xguid.hpp
@@ -0,0 +1,48 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XGUID_HPP
+#define XGUID_HPP
+
+#include <cstddef>
+#include <array>
+#include <string>
+#include "xeus_export.hpp"
+
+namespace xeus
+{
+    class XEUS_API xguid
+    {
+
+    public:
+
+        static constexpr std::size_t GUID_SIZE = 16;
+        using buffer_type = std::array<unsigned char, GUID_SIZE>;
+
+        xguid(const char* buffer);
+        xguid(const buffer_type& buffer);
+
+        const unsigned char* buffer() const;
+        static constexpr std::size_t buffer_size()
+        {
+            return GUID_SIZE;
+        }
+
+        std::string to_string() const;
+
+    private:
+
+        buffer_type m_buffer;
+    };
+
+    XEUS_API
+    xguid new_xguid();
+
+}
+
+#endif
---
+++ src/xguid.cpp
@@ -0,0 +1,120 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include "xguid.hpp"
+
+#ifdef GUID_LIBUUID
+#include <uuid/uuid.h>
+#endif
+
+#ifdef GUID_CFUUID
+#include <CoreFoundation/CFUUID.h>
+#endif
+
+#ifdef GUID_WINDOWS
+#include <objbase.h>
+#endif
+
+namespace xeus
+{
+
+    xguid::xguid(const char* buffer)
+    {
+        std::copy(buffer, buffer + GUID_SIZE, m_buffer.begin());
+    }
+
+    xguid::xguid(const buffer_type& buffer)
+        : m_buffer(buffer)
+    {
+    }
+
+    const unsigned char* xguid::buffer() const
+    {
+        return m_buffer.data();
+    }
+
+    std::string xguid::to_string() const
+    {
+        std::string res(buffer(), buffer() + buffer_size());
+        return res;
+    }
+
+#ifdef GUID_LIBUUID
+
+    xguid new_xguid()
+    {
+        uuid_t id;
+        uuid_generate(id);
+        return id;
+    }
+#endif
+
+#ifdef GUID_CFUUID
+
+    xguid new_xguid()
+    {
+        auto id = CFUUIDCreate(NULL);
+        auto bytes = CFUUIDGetUUIDBytes(id);
+        CFRelease(id);
+
+        std::array<unsigned char, 16> buffer =
+        {
+            bytes.byte0,
+            bytes.byte1,
+            bytes.byte2,
+            bytes.byte3,
+            bytes.byte4,
+            bytes.byte5,
+            bytes.byte6,
+            bytes.byte7,
+            bytes.byte8,
+            bytes.byte9,
+            bytes.byte10,
+            bytes.byte11,
+            bytes.byte12,
+            bytes.byte13,
+            bytes.byte14,
+            bytes.byte15
+        };
+        return buffer;
+    }
+
+#endif
+
+#ifdef GUID_WINDOWS
+
+    xguid new_xguid()
+    {
+        GUID id;
+        CoCreateGuid(&id);
+
+        std::array<unsigned char, 16> buffer =
+        {
+            (id.Data1 >> 24) & 0xFF,
+            (id.Data1 >> 16) & 0xFF,
+            (id.Data1 >> 8) & 0xFF,
+            (id.Data1) & 0xFF,
+            (id.Data2 >> 8) & 0xFF,
+            (id.Data2) & 0xFF,
+            (id.Data3 >> 8) & 0xFF,
+            (id.Data3) & 0xFF,
+            id.Data4[0],
+            id.Data4[1],
+            id.Data4[2],
+            id.Data4[3],
+            id.Data4[4],
+            id.Data4[5],
+            id.Data4[6],
+            id.Data4[7]
+        };
+
+        return buffer;
+    }
+#endif
+
+}
