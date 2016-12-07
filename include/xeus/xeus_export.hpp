4f9611f69cc543c981987f8c7592ec557c3104e3
Johan Mabille <johan.mabille@gmail.com>
2016-12-07 15:43:20 +0100

    symbols export

---
+++ include/xeus/xeus_export.hpp
@@ -0,0 +1,22 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XEUS_EXPORT_HPP
+#define XEUS_EXPORT_HPP
+
+#ifdef _WIN32
+    #ifdef XEUS_EXPORTS
+        #define XEUS_API __declspec(dllexport)
+    #else
+        #define XEUS_API __declspec(dllimport)
+    #endif
+#else
+    #define XEUS_API
+#endif
+
+#endif
