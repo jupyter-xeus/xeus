4fc34be2d4118580215e87218042727ed2e7e6d4
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-03-20 16:35:42 +0100

    Release 0.19.0

--- include/xeus/xeus.hpp
+++ include/xeus/xeus.hpp
@@ -25,8 +25,8 @@
 
 // Project version
 #define XEUS_VERSION_MAJOR 0
-#define XEUS_VERSION_MINOR 18
-#define XEUS_VERSION_PATCH 1
+#define XEUS_VERSION_MINOR 19
+#define XEUS_VERSION_PATCH 0
 
 // Binary version
 #define XEUS_BINARY_CURRENT 1
