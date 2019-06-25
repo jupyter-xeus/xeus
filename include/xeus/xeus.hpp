0b7626f5409cf78803f6f9b3dcc9fcc4ba88d18f
Johan Mabille <johan.mabille@gmail.com>
2019-06-25 17:59:09 +0200

    Release 0.19.3

--- include/xeus/xeus.hpp
+++ include/xeus/xeus.hpp
@@ -26,7 +26,7 @@
 // Project version
 #define XEUS_VERSION_MAJOR 0
 #define XEUS_VERSION_MINOR 19
-#define XEUS_VERSION_PATCH 2
+#define XEUS_VERSION_PATCH 3
 
 // Binary version
 #define XEUS_BINARY_CURRENT 1
