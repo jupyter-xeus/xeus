504ede203f915958968d3dd24aad6eae0df0fb05
Thibault Lacharme <thibault.lacharme2@gmail.com>
2019-07-11 15:57:05 +0200

    implementing get_username for windows

---
+++ test/test_unit_kernel.cpp
@@ -0,0 +1,29 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille, Sylvain Corlay and Wolf Vollprecht    *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include "gtest/gtest.h"
+
+#include <string>
+
+#include "nlohmann/json.hpp"
+
+#include "xeus/xkernel.hpp"
+
+namespace nl = nlohmann;
+
+namespace xeus
+{
+    TEST(kernel, get_username)
+    {
+        std::string username;
+        username = get_user_name();
+        std::cout << "Username : " << username << std::endl;
+        EXPECT_NE(username, "unspecified user");
+    }
+}
+
