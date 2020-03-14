9d64bf8c689f5b1939a71bbc82ececfeb8ab39f2
Johan Mabille <johan.mabille@gmail.com>
2020-03-14 23:51:10 +0100

    Release 0.23.8

--- include/xeus/xeus.hpp
+++ include/xeus/xeus.hpp
@@ -27,7 +27,7 @@
 // Project version
 #define XEUS_VERSION_MAJOR 0
 #define XEUS_VERSION_MINOR 23
-#define XEUS_VERSION_PATCH 7
+#define XEUS_VERSION_PATCH 8
 
 // Binary version
 #define XEUS_BINARY_CURRENT 1
