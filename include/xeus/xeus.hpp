3d0ec507fe1d8a9d5dd1f8df89376169bbf7f7be
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-03-27 15:06:09 +0100

    Release 0.23.10

--- include/xeus/xeus.hpp
+++ include/xeus/xeus.hpp
@@ -27,7 +27,7 @@
 // Project version
 #define XEUS_VERSION_MAJOR 0
 #define XEUS_VERSION_MINOR 23
-#define XEUS_VERSION_PATCH 9
+#define XEUS_VERSION_PATCH 10
 
 // Binary version
 #define XEUS_BINARY_CURRENT 1
