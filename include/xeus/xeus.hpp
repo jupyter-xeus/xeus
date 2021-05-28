c61b55a8aa2394c4f5c4b8eecdf3687236df7448
Johan Mabille <johan.mabille@gmail.com>
2021-05-28 15:50:39 +0200

    Release 1.0.2

--- include/xeus/xeus.hpp
+++ include/xeus/xeus.hpp
@@ -27,12 +27,12 @@
 // Project version
 #define XEUS_VERSION_MAJOR 1
 #define XEUS_VERSION_MINOR 0
-#define XEUS_VERSION_PATCH 1
+#define XEUS_VERSION_PATCH 2
 
 // Binary version
-#define XEUS_BINARY_CURRENT 1
-#define XEUS_BINARY_REVISION 1
-#define XEUS_BINARY_AGE 0
+#define XEUS_BINARY_CURRENT 2
+#define XEUS_BINARY_REVISION 0
+#define XEUS_BINARY_AGE 1
 
 // Kernel protocol version
 #define XEUS_KERNEL_PROTOCOL_VERSION_MAJOR 5
