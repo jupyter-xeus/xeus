51a2e241dd89200bb423832796f3e1b8fa47da67
Mariana Meireles <mariana@psychonautgirl.space>
2021-09-15 17:21:20 +0200

    Add XEUS_API prefix to builder methods

--- include/xeus/xmessage_builder.hpp
+++ include/xeus/xmessage_builder.hpp
@@ -20,38 +20,46 @@ namespace nl = nlohmann;
 
 namespace xeus
 {
-    nl::json create_error_reply(const std::string& ename = std::string(),
-                                const std::string& evalue = std::string(),
-                                const nl::json& trace_back = nl::json::array());
+    class XEUS_API message_builder
+    {};
+        XEUS_API
+        nl::json create_error_reply(const std::string& ename = std::string(),
+                                    const std::string& evalue = std::string(),
+                                    const nl::json& trace_back = nl::json::array());
 
-    nl::json create_successful_reply(const nl::json& payload = nl::json::array(),
-                                     const nl::json& user_expressions = nl::json::object());
+        XEUS_API
+        nl::json create_successful_reply(const nl::json& payload = nl::json::array(),
+                                         const nl::json& user_expressions = nl::json::object());
 
-    nl::json create_complete_reply(const nl::json& matches,
-                                   const int& cursor_start,
-                                   const int& cursor_end,
-                                   const nl::json& metadata = nl::json::object());
+        XEUS_API
+        nl::json create_complete_reply(const nl::json& matches,
+                                       const int& cursor_start,
+                                       const int& cursor_end,
+                                       const nl::json& metadata = nl::json::object());
 
-    nl::json create_inspect_reply(const bool found = false,
-                                  const nl::json& data = nl::json::object(),
-                                  const nl::json& metadata = nl::json::object());
+        XEUS_API
+        nl::json create_inspect_reply(const bool found = false,
+                                      const nl::json& data = nl::json::object(),
+                                      const nl::json& metadata = nl::json::object());
 
-    nl::json create_is_complete_reply(const std::string& status = std::string(),
-                                      const std::string& indent = std::string(""));
+        XEUS_API
+        nl::json create_is_complete_reply(const std::string& status = std::string(),
+                                          const std::string& indent = std::string(""));
 
-    nl::json create_info_reply(const std::string& protocol_version = std::string(),
-                               const std::string& implementation = std::string(),
-                               const std::string& implementation_version = std::string(),
-                               const std::string& language_name = std::string(),
-                               const std::string& language_version = std::string(),
-                               const std::string& language_mimetype = std::string(),
-                               const std::string& language_file_extension = std::string(),
-                               const std::string& pygments_lexer = std::string(),
-                               const std::string& language_codemirror_mode = std::string(),
-                               const std::string& language_nbconvert_exporter = std::string(),
-                               const std::string& banner = std::string(),
-                               const bool debugger = false,
-                               const nl::json& help_links = nl::json::object());
+        XEUS_API
+        nl::json create_info_reply(const std::string& protocol_version = std::string(),
+                                   const std::string& implementation = std::string(),
+                                   const std::string& implementation_version = std::string(),
+                                   const std::string& language_name = std::string(),
+                                   const std::string& language_version = std::string(),
+                                   const std::string& language_mimetype = std::string(),
+                                   const std::string& language_file_extension = std::string(),
+                                   const std::string& pygments_lexer = std::string(),
+                                   const std::string& language_codemirror_mode = std::string(),
+                                   const std::string& language_nbconvert_exporter = std::string(),
+                                   const std::string& banner = std::string(),
+                                   const bool debugger = false,
+                                   const nl::json& help_links = nl::json::object());
 }
 
 #endif
