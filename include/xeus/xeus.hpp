96923bc1a5d9899412e2b6356609c39d2888c053
Johan Mabille <johan.mabille@gmail.com>
2022-12-26 14:23:58 +0100

    Release 3.0.4

--- include/xeus/xeus.hpp
+++ include/xeus/xeus.hpp
@@ -27,7 +27,7 @@
 // Project version
 #define XEUS_VERSION_MAJOR 3
 #define XEUS_VERSION_MINOR 0
-#define XEUS_VERSION_PATCH 3
+#define XEUS_VERSION_PATCH 4
 
 // Binary version
 #define XEUS_BINARY_CURRENT 8
