0270a1bad9e52ff47a712f3619780296c557e0a7
Ian Thomas <ianthomas23@gmail.com>
2025-04-10 15:07:54 +0100

    Remove USE_ES6_IMPORT_META setting that is no longer supported

--- cmake/WasmBuildOptions.cmake
+++ cmake/WasmBuildOptions.cmake
@@ -26,7 +26,6 @@ function(xeus_wasm_link_options target environment)
         PUBLIC "SHELL: -s MODULARIZE=1"
         PUBLIC "SHELL: -s EXPORT_NAME=\"createXeusModule\""
         PUBLIC "SHELL: -s EXPORT_ES6=0"
-        PUBLIC "SHELL: -s USE_ES6_IMPORT_META=0"
         PUBLIC "SHELL: -s ASSERTIONS=0"
         PUBLIC "SHELL: -s ALLOW_MEMORY_GROWTH=1"
         PUBLIC "SHELL: -s EXIT_RUNTIME=1"
