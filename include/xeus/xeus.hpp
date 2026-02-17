fbdda458dece2378d42c6b0908921352f1d4d655
Johan Mabille <johan.mabille@gmail.com>
2026-02-17 16:15:40 +0100

    Release 5.2.8

--- include/xeus/xeus.hpp
+++ include/xeus/xeus.hpp
@@ -27,13 +27,13 @@
 // Project version
 #define XEUS_VERSION_MAJOR 5
 #define XEUS_VERSION_MINOR 2
-#define XEUS_VERSION_PATCH 7
+#define XEUS_VERSION_PATCH 8
 
 // Binary version
 // See the following URL for explanations about the binary versionning
 // https://www.gnu.org/software/libtool/manual/html_node/Updating-version-info.html#Updating-version-info
 #define XEUS_BINARY_CURRENT 13
-#define XEUS_BINARY_REVISION 5
+#define XEUS_BINARY_REVISION 6
 #define XEUS_BINARY_AGE 0
 
 // Kernel protocol version
