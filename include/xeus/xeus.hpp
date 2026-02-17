4912ac9e64bd6daaedf077231b674a4d57633669
Johan Mabille <johan.mabille@gmail.com>
2026-02-17 16:03:16 +0100

    Updated protocol version to 5.5

--- include/xeus/xeus.hpp
+++ include/xeus/xeus.hpp
@@ -38,7 +38,7 @@
 
 // Kernel protocol version
 #define XEUS_KERNEL_PROTOCOL_VERSION_MAJOR 5
-#define XEUS_KERNEL_PROTOCOL_VERSION_MINOR 3
+#define XEUS_KERNEL_PROTOCOL_VERSION_MINOR 5
 
 // Composing the protocol version string from major, and minor
 #define XEUS_CONCATENATE(A, B) XEUS_CONCATENATE_IMPL(A, B)
